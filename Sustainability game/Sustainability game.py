f26fc7c9642ab1ed6ea266b8fc6a65ec71ca4a48
Isaac <isaac.rice@gihs.sa.edu.au>
2020-08-28 14:43:02 +0930

    Cum everywhere

--- Sustainability game/Sustainability game.py
+++ Sustainability game/Sustainability game.py
@@ -20,6 +20,35 @@ MAGENTA = (255, 0, 255)
 SILVER = (192, 192, 192)
 
 
+class Game:
+    def __init__(self):
+        self.done = False
+        self.size = (1000, 500)
+        self.screen = pygame.display.set_mode(self.size)
+        self.done = False
+        self.clock = pygame.time.Clock()
+
+    def do(self):
+        while not self.done:
+            # --- Main event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.done = True
+
+            # --- Game logic should go here
+
+            # --- Screen-clearing code goes here
+
+            # If you want a background image, replace this clear with blit'ing thebackground image.
+            self.screen.fill(WHITE)
+
+            # --- Drawing code should go here
+
+            pygame.display.flip()
+
+            self.clock.tick(60)
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -36,43 +65,11 @@ class Player(pygame.sprite.Sprite):
 
 pygame.init()
 
-# Set the width and height of the screen [width, height]
-size = (1000, 500)
-screen = pygame.display.set_mode(size)
-
 pygame.display.set_caption("My Game")
 
-# Loop until the user clicks the close button.
-done = False
+game = Game()
 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-    # --- Game logic should go here
-
-    # --- Screen-clearing code goes here
-
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
-    screen.fill(WHITE)
-
-    # --- Drawing code should go here
-
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-
-    # --- Limit to 60 frames per second
-    clock.tick(60)
+game.do()
 
 # Close the window and quit.
 pygame.quit()
